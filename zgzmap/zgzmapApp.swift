7f1908c570f71afc8336bd3b809e23785332579f
Daniel Vela <veladan@me.com>
2022-10-04 08:18:56 +0200

    Initial Commit

---
+++ zgzmap/zgzmapApp.swift
@@ -0,0 +1,17 @@
+//
+//  zgzmapApp.swift
+//  zgzmap
+//
+//  Created by Daniel Vela on 4/10/22.
+//
+
+import SwiftUI
+
+@main
+struct zgzmapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
